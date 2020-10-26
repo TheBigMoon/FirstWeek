7994134e192d63a63b068c80e2b5177a16c7be0d
Danium <Kividanya964@gmail.com>
2020-10-26 17:40:28 +0300

    DaniumPackage is added

--- Base/Files/src/js/bootstrap.js
+++
@@ -1,8 +0,0 @@
-(function() {
-	require.config({
-		paths: {
-			"DriverJs": Terrasoft.getFileContentUrl("Base", "src/js/driver.min.js"),
-			"DriverCSS": Terrasoft.getFileContentUrl("Base", "src/css/driver.min.css"),
-		}
-	});
-})();
---
+++ DaniumPackage/Schemas/UsrDaniumCategory705061b7Section/UsrDaniumCategory705061b7Section.js
@@ -0,0 +1,8 @@
+define("UsrDaniumCategory705061b7Section", [], function() {
+	return {
+		entitySchemaName: "UsrDaniumCategory",
+		details: /**SCHEMA_DETAILS*/{}/**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/[]/**SCHEMA_DIFF*/,
+		methods: {}
+	};
+});
